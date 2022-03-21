d6467c54d9cbbfadb40a0a5f9da83e99c899be08
Michel Meyer <syrion@outlook.com>
2022-03-21 15:15:46 +0100

    ajout param pour que l'article a supprimer le soit par l'utilisateur qui à poster l'article.

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -11,15 +11,40 @@ exports.createSauce = (req, res, next) => {
 };
 
 exports.modifySauce = (req, res, next) => {
-  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+  Sauce.updateOne({ _id: req.params.id }, 
+    { ...req.body, _id: req.params.id })
     .then(() => res.status(200).json({ message: "Objet modifié !" }))
     .catch((error) => res.status(400).json({ error }));
 };
 
 exports.deleteSauce = (req, res, next) => {
-  Sauce.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet supprimé !" }))
-    .catch((error) => res.status(400).json({ error }));
+  Sauce.findOne({ _i: req.params.id}).then(     // On récupère la sauce
+    (sauce) => {
+      if (!sauce) {
+        return res.status(404).json({
+          error: new Error('Objet non trouvé !')
+        });
+      }
+      if (sauce.userId !== req.auth.userId) {   // On vérifie que la sauce appartiens bien a celui qui fait la requête
+        return res.status(401).json({
+          error: new Error('Requête non autorisée !')
+        });
+      }
+      Sauce.deleteOne({ _id: req.params.id }).then(  // Si oui, on la supprime
+        () => {
+          res.status(200).json({ 
+            message: "Objet supprimé !" 
+          });
+        }
+    ).catch(
+      (error) => {
+        res.status(400).json({ 
+          error: error 
+        });
+      }
+    );
+    }
+  );
 };
 
 exports.findOneSauce = (req, res, next) => {
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,9 +2,10 @@ const jwt = require("jsonwebtoken"); // pour vérifier les tokens
 
 module.exports = (req, res, next) => {
     try {
-      const token = req.headers.authorization.split(' ')[1];
+      const token = req.headers.authorization.split(' ')[1]; // On récupère le token et on le décode 
       const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
       const userId = decodedToken.userId;
+      req.auth = { userId };
       if ( req.body.userId && req.body.userId !== userId) {
           throw 'User ID non valable ! '
       } else {
